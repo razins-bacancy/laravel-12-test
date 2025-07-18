110ba6b93a712b8ffb489082f5185260650c530a
razins-bacancy <mohammedrazin.shaikh@bacancy.com>
2025-07-18 14:13:56 +0530

    dev command for development quick r&d purpose

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+if (app()->isLocal()) {
+    Artisan::command('dev', function () {
+        //
+    })->purpose('For development r&d purposes only.');
+}
